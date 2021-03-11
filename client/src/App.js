da7e4e7c3768bcc7671c2df537f5298f565ad619
Matt Windle <matthewwindle@outlook.com>
2021-03-11 22:22:26 +0000

    setup api for get request and sent to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,63 @@
+import { useState, useEffect } from "react";
 import './App.css';
+import Axios  from "axios";
 
 function App() {
+
+const [movieName, setMovieName] = useState('');
+const [review, setReview] = useState("");
+const [movieReviewList, setMovieReviewList] = useState([]);
+
+const submitReview = (e) => {
+  console.log("submit");
+  Axios.post('http://localhost:3001/api/insert', {
+  movieName: movieName,
+  movieReview: review,
+  }).then(() => {
+    alert("Successfull");
+  })
+};
+
+
+useEffect(() => {
+  Axios.get('http://localhost:3001/api/get').then((response) => {
+    setMovieReviewList(response.data)
+  });
+}, []);
+
   return (
-    <div className="App">
+    <div className="App"> 
       <form action="">
         <label htmlFor="">Movie Name</label>
-        <input type="text"/>
+        <input 
+          type="text" 
+          name="movieName" 
+          onChange={(e) => {
+          setMovieName(e.target.value);
+          }} 
+        />
         <label htmlFor="">Movie review</label>
-        <input type="text"/>
-        <button>Send</button>
+        <input 
+          type="text" 
+          name="movieReview" 
+          onChange={(e) => { 
+          setReview(e.target.value);
+          }} 
+        />
+        <button type="submit" onClick={submitReview} >Send</button>
       </form>
+
+      <ul>
+      {movieReviewList.map((data) => {
+        return ( 
+          <li key={data.id}>
+          <p>Movie Name : {data.movieName}</p>
+          <p>Review: {data.movieReview}</p>
+          </li>
+        )
+      })}
+      </ul>
+
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
 
@@ -9,17 +11,26 @@ const db = mysql.createPool({
     database: 'reactcrud',
 });
 
-
-app.get('/', (req, res) => {
-    
-    // const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('LOTR', 'Excellent')";
-    // db.query(sqlInsert, (err, result) => {
-    //     res.send("hello world")
-    // });
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
+app.post('/api/insert', (req, res) => {
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+    db.query(sqlInsert, [movieName, movieReview], (err, results) => {
+        console.log(results);
+    })
 }) 
 
+app.get("/api/get", (req, res) => {
+    const sqlSelect = "SELECT * FROM movie_reviews";
+    db.query(sqlSelect, (err, results) => {
+        res.send(results)
+    })
+})
 
 app.listen(3001, () => {
     console.log("running")
